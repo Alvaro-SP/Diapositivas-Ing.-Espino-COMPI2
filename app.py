1f829a93695f3f0ff4a8cd616fca09eee714219e
Alvaro-SP <socop2412@gmail.com>
2022-06-28 20:48:37 -0600

    data science...

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ from ast import For
     # print("![](http://kunusoft.com/slides/compi2/olc207_parcheo/Diapositiva0"+str(i)+".JPG)")
 # for i in range(52):
 #     print("![](http://kunusoft.com/slides/compi2/olc209_optimizacion/Diapositiva0"+str(i)+".JPG)")
-for i in range(31):
-    print("![](https://kunusoft.com/slides/ciencia_datos/1intro/Diapositiva0"+str(i)+".JPG)")
+for i in range(37):
+    print("![](https://kunusoft.com/slides/ciencia_datos/2explora/Diapositiva0"+str(i)+".JPG)")
