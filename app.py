6eb289fb1b97ecd385072ef167b7297cbeba9767
Alvaro-SP <socop2412@gmail.com>
2022-06-28 20:53:53 -0600

    ultimo data science fuentes: Ing Espino

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ from ast import For
     # print("![](http://kunusoft.com/slides/compi2/olc207_parcheo/Diapositiva0"+str(i)+".JPG)")
 # for i in range(52):
 #     print("![](http://kunusoft.com/slides/compi2/olc209_optimizacion/Diapositiva0"+str(i)+".JPG)")
-for i in range(37):
-    print("![](https://kunusoft.com/slides/ciencia_datos/2explora/Diapositiva0"+str(i)+".JPG)")
+# for i in range(37):
+#     print("![](https://kunusoft.com/slides/ciencia_datos/2explora/Diapositiva0"+str(i)+".JPG)")
+# for i in range(38):
+#     print("![](https://kunusoft.com/slides/ciencia_datos/3regre/Diapositiva0"+str(i)+".JPG)")
+for i in range(42):
+    print("![](https://kunusoft.com/slides/ciencia_datos/4clasifica/Diapositiva0"+str(i)+".JPG)")
