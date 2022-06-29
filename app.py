37553f8b7802c1d1023b052c1c047d7b33ebefb0
Alvaro-SP <socop2412@gmail.com>
2022-06-28 20:46:03 -0600

    new commit

--- app.py
+++ app.py
@@ -6,5 +6,7 @@ from ast import For
 #     print("![](http://kunusoft.com/slides/compi2/olc206_tipos_flujo/Diapositiva0"+str(i)+".JPG)")
 # for i in range(52):
     # print("![](http://kunusoft.com/slides/compi2/olc207_parcheo/Diapositiva0"+str(i)+".JPG)")
-for i in range(52):
-    print("![](http://kunusoft.com/slides/compi2/olc209_optimizacion/Diapositiva0"+str(i)+".JPG)")
+# for i in range(52):
+#     print("![](http://kunusoft.com/slides/compi2/olc209_optimizacion/Diapositiva0"+str(i)+".JPG)")
+for i in range(31):
+    print("![](https://kunusoft.com/slides/ciencia_datos/1intro/Diapositiva0"+str(i)+".JPG)")
