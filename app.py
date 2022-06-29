f413a6c728d06f68380094b28fd8d1d3ba9477c9
Alvaro-SP <socop2412@gmail.com>
2022-06-28 20:38:39 -0600

    commit

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ from ast import For
 # for i in range(52):
     # print("![](http://kunusoft.com/slides/compi2/olc207_parcheo/Diapositiva0"+str(i)+".JPG)")
 for i in range(52):
-    print("![](http://kunusoft.com/slides/compi2/olc208_entorno/Diapositiva0"+str(i)+".JPG)")
+    print("![](http://kunusoft.com/slides/compi2/olc209_optimizacion/Diapositiva0"+str(i)+".JPG)")
